6678da1be5bfb28a4cbd2c4e72b092c1aaf491bb
Chris <chris_andrews_1994@outlook.com>
2025-10-02 20:52:08 +0100

    Add basic powerups

--- super-exciting-game-project/scenes/main_character.gd
+++ super-exciting-game-project/scenes/main_character.gd
@@ -4,15 +4,38 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+# Powerups
+# TODO: Move power ups to their own class/scene
+const double_jump = "pw_double_jump"
+const tripple_jump = "pw_tripple_jump"
+const slow_fall = "pw_slow_fall"
+
+var active_power = ""
+var remaining_jumps = 1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
+		var gravity_modifier = 1
+		if get_active_power() == slow_fall:
+			gravity_modifier = 0.8
 		velocity += get_gravity() * delta
+		if velocity.y > 0:
+			velocity.y *= gravity_modifier
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			match active_power:
+				double_jump:
+					remaining_jumps = 2
+				tripple_jump:
+					remaining_jumps = 3
+				_:
+					remaining_jumps = 1
+		if remaining_jumps > 0:
+			remaining_jumps -= 1
+			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -21,5 +44,25 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	#TODO: Temp button to cycle through powerups, remove later
+	if Input.is_action_just_pressed("ui_page_up"):
+		cycle_power()
 	move_and_slide()
+
+func get_active_power() -> String:
+	return active_power
+
+func set_active_power(power: String) -> void:
+	active_power = power
+	
+func cycle_power() -> void:
+	match get_active_power():
+		double_jump:
+			set_active_power(tripple_jump)
+		tripple_jump:
+			set_active_power(slow_fall)
+		slow_fall:
+			set_active_power("")
+		_:
+			set_active_power(double_jump)
